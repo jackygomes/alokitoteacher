c59826988fde1efc58b5b3e0c2e412168671c95a
Devil Eye <jacky21994@gmail.com>
2020-08-25 12:26:16 +0600

    purchase disable for free content

--- resources/views/resource/overview.blade.php
+++ resources/views/resource/overview.blade.php
@@ -60,22 +60,28 @@
                     @endif
                 </span></span>
                 <br>
-                @if($info->isBought == 1 && Auth::check())
-                <a href="{{ url('view') }}/{{ Request::segment(2) }}/{{ Request::segment(3) }}" class="mt-4 btn btn-success btn-lg">
-                    View Resource
-                </a>
-                @elseif(Auth::check())
-                    @if($info->price > Auth::user()->balance)
-                        <form onclick="return confirm('Insufficiant Balance. Deposit your balance first.')">
-                            @csrf
-                            <button type="submit" class="mt-4 btn btn-success btn-lg">Purchase</button>
-                        </form>
-                    @else
-                        <form action="{{route('purchase.product', $info->id)}}" onclick="return confirm('Are you sure to purchase this resource? if yes then click ok.')" method="post">
-                            @csrf
-                            <input type="hidden" name="type" value="resource">
-                            <button type="submit" class="mt-4 btn btn-success btn-lg">Purchase</button>
-                        </form>
+                @if($info->price == 0)
+                    <a href="{{ url('view') }}/{{ Request::segment(2) }}/{{ Request::segment(3) }}" class="mt-4 btn btn-success btn-lg">
+                        View Resource
+                    </a>
+                @else
+                    @if($info->isBought == 1 && Auth::check())
+                    <a href="{{ url('view') }}/{{ Request::segment(2) }}/{{ Request::segment(3) }}" class="mt-4 btn btn-success btn-lg">
+                        View Resource
+                    </a>
+                    @elseif(Auth::check())
+                        @if($info->price > Auth::user()->balance)
+                            <form onclick="return confirm('Insufficiant Balance. Deposit your balance first.')">
+                                @csrf
+                                <button type="submit" class="mt-4 btn btn-success btn-lg">Purchase</button>
+                            </form>
+                        @else
+                            <form action="{{route('purchase.product', $info->id)}}" onclick="return confirm('Are you sure to purchase this resource? if yes then click ok.')" method="post">
+                                @csrf
+                                <input type="hidden" name="type" value="resource">
+                                <button type="submit" class="mt-4 btn btn-success btn-lg">Purchase</button>
+                            </form>
+                        @endif
                     @endif
                 @endif
 
