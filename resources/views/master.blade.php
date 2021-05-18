855162fca65f278d63ed0d95f0ce1bba66f8d34b
Devil Eye <jacky21994@gmail.com>
2021-05-18 12:10:24 +0600

    frontend change

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
     <link rel="stylesheet" href="{{URL::asset('css/bootstrap-datepicker.min.css')}}">
     <link rel="stylesheet" href="{{URL::asset('css/style1.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('css/custom.css')}}">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178521926-1"></script>
     <script>
@@ -71,29 +72,20 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
 
-            <ul class="navbar-nav ml-auto mr-3">
-
+            <ul class="navbar-nav mr-auto ml-2">
 
                 <li class="nav-item active">
                     <a class="nav-link text-black" href="{{ url('/') }}">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
-                {{--                @auth--}}
                 <li class="nav-item">
-                    {{--                    @if(Auth::user()->identifier == 1 || Auth::user()->identifier == 2 || Auth::user()->identifier == 101)--}}
                     <a class="nav-link text-black" href="{{ route('jobBoard') }}">Job Board</a>
-                    {{--                    @endif--}}
                 </li>
-                {{--                @endauth--}}
                 <li class="nav-item">
                     <a class="nav-link text-black" href="{{ url('all') }}">Toolkits/Course</a>
                 </li>
-                <!--
-                <li class="nav-item">
-                    <a class="nav-link text-black" href="@if(Auth::check() && Auth::user()->identifier == 1) {{ url('jobs/all') }} @else {{ url('jobs') }} @endif">Jobs</a>
-                </li>
--->
+
 
                 <li class="nav-item">
 
@@ -101,10 +93,16 @@
 
                 </li>
 
+
+            </ul>
+            <ul class="navbar-nav ml-auto ml-2">
                 @guest
-                <li class="nav-item">
-                    <a class="nav-link text-black" href="{{ url('login') }}">Login</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn px-4 mr-2 text-black login-button" href="{{ url('login') }}">Login</a>
+                    </li>
+                    <li>
+                        <a class="nav-link btn background-yellow px-4 text-white" href="{{ url('login') }}">Sign Up</a>
+                    </li>
                 @endguest @auth
                 <div class="btn-group ml-2">
                     <button type="button" class="btn background-yellow text-white dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -112,14 +110,14 @@
                     </button>
                     <div class="dropdown-menu">
                         @if(Auth::user()->identifier == 101 || Auth::user()->identifier == 104)
-                        <a class="dropdown-item" href="@if(Auth::user()->identifier == 101 || Auth::user()->identifier == 104) {{ url('dashboard') }}@endif">Dashboard</a>
+                            <a class="dropdown-item" href="@if(Auth::user()->identifier == 101 || Auth::user()->identifier == 104) {{ url('dashboard') }}@endif">Dashboard</a>
                         @else
                             @if(Auth::user()->identifier == 1)
                                 <a class="dropdown-item" href="{{url('t/dashboard')}}">Dashboard</a>
                             @elseif(Auth::user()->identifier == 2)
                                 <a class="dropdown-item" href="{{url('s/dashboard')}}">Dashboard</a>
                             @endif
-                        <a class="dropdown-item" href="@if(Auth::user()->identifier == 1){{ url('t') }}@elseif(Auth::user()->identifier == 2) {{ url('s') }}@elseif(Auth::user()->identifier == 4) {{ url('stu') }}@endif/{{ Auth::user()->username }}">Profile</a>
+                            <a class="dropdown-item" href="@if(Auth::user()->identifier == 1){{ url('t') }}@elseif(Auth::user()->identifier == 2) {{ url('s') }}@elseif(Auth::user()->identifier == 4) {{ url('stu') }}@endif/{{ Auth::user()->username }}">Profile</a>
                         @endif
                         <a class="dropdown-item" href="{{ url('settings') }}">Settings</a>
                         <form action="{{ url('logout') }}" method="POST">
@@ -128,11 +126,8 @@
                         </form>
                     </div>
 
-
                 </div>
                 @endauth
-
-
             </ul>
         </div>
     </nav>
@@ -142,49 +137,60 @@
 
 
     <!-- Start of Footer area -->
-    <footer class="page-footer font-small special-color-dark pt-4 bg-dark">
+    <footer class="page-footer font-small special-color-dark">
 
         <div class="container">
             <div class="row">
-                <div class="col-md-3 text-center">
-                    <h4 class="font-weight-bold text-white">QUICK LINKS</h4>
+                <div class="col-md-4">
+                    <div class="logo mb-5">
+                        <a class="alokitologo" href="{{ url('/') }}"><img src="{{asset('images\logo\alokito_logo.png')}}"></a>
+                    </div>
+                    <div class="social-icon pt-4">
+                        <a href="https://www.facebook.com/Alokito-Teachers-1022969321242132" target="_blank"><i class="fab fa-facebook fa-2x mr-2 custom-hover2" ></i></a>
+                        <a href="https://www.youtube.com/channel/UCY4PBN9HLG5oxjCtYXGvfeg" target="_blank"><i class="fab fa-youtube fa-2x mr-2 custom-hover2"></i></a>
+                        <a href="https://www.linkedin.com/company/14756318/" target="_blank"><i class="fab fa-linkedin fa-2x mr-2 custom-hover2"></i></a>
+                        <a href="#" target="_blank"><i class="fab fa-instagram fa-2x mr-2 custom-hover2"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <h4 class="font-weight-bold">QUICK LINKS</h4>
                     <ul class="list-unstyled">
-                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('about_us') }}">About us</a></li>
-                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('/') }}">Alokito Journey</a></li>
-                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('/book_workshop') }}">Book a workshop</a></li>
-                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('toolkit') }}">Toolkits</a></li>
-                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('course') }}">Courses</a></li>
-                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('contact_us') }}">Contact Us</a></li>
+                        <li> <a class="" href="{{ url('about_us') }}">About us</a></li>
+                        <li> <a class="" href="{{ url('/') }}">Alokito Journey</a></li>
+{{--                        <li> <a class="font-weight-bold text-orange custom-hover" href="{{ url('/book_workshop') }}">Book a workshop</a></li>--}}
+                        <li> <a class="" href="{{ url('toolkit') }}">Toolkits</a></li>
+                        <li> <a class="" href="{{ url('course') }}">Courses</a></li>
+                        <li> <a class="" href="{{ url('contact_us') }}">Contact Us</a></li>
                     </ul>
                 </div>
 
 
 
                 <div class="col-md-3 text-center">
-                    <h4 class="font-weight-bold text-white">GET IN TOUCH</h4>
-                    <p class="font-weight-bold text-orange">3/11, Block B, Lalmatia, Dhaka 1207, Bangladesh</p>
+                    <h4 class="font-weight-bold">GET IN TOUCH</h4>
                     <ul class="list-unstyled mb-5">
                         <li>
-                            <a class="mt-3 font-weight-bold text-orange custom-hover" href="tel:+8801742812044">+880-1742812044</a>
+                            <a class="mt-3" href="tel:+8801742812044">+880-1742812044</a>
                         </li>
                         <li>
-                            <a class="mt-3 font-weight-bold text-orange custom-hover" href="mailto:info@alokitoteachers.com">info@alokitoteachers.com</a>
+                            <a class="mt-3" href="mailto:info@alokitoteachers.com">info@alokitoteachers.com</a>
                         </li>
                     </ul>
+                    <p class="">3/11, Block B, Lalmatia</p>
 
                 </div>
 
 
                 <div class="col-md-3">
-                    <h4 class="text-white text-center font-weight-bold">SUBSCRIBE TO OUR MAILING LIST</h4>
+                    <h4 class=" font-weight-bold">SUBSCRIBE TO US</h4>
                     <form action="{{ url('email_subscribe') }}" method="POST">
                         {{ csrf_field() }}
                         <div class="form-group row">
                             <div class="col-sm-3 col-md-12">
-                                <div class="text-center">
+                                <div class="">
                                     <form action="{{ url('email_subscribe') }}" method="POST">
                                         <input type="email" name="email" class="form-control" placeholder="Your Email Address">
-                                        <button type="submit" class="mt-3 btn background-yellow px-2 py-2 shadow font-weight-bold text-dark">SUBSCRIBE</button>
+                                        <button type="submit" class="mt-3 btn background-yellow px-4 py-2 shadow font-weight-bold text-white">SUBSCRIBE</button>
 
                                     </form>
 
@@ -194,35 +200,24 @@
                     </form>
                 </div>
 
-                <div class="col-md-3 text-center">
-                    <h4 class="font-weight-bold text-white" id="footer-social">STAY CONNECTED</h4>
-                    <div class="social-icon mb-5">
-                        <a href="https://www.facebook.com/Alokito-Teachers-1022969321242132" target="_blank"><i class="fab fa-facebook fa-2x mr-2 custom-hover2" style="color:#3b5998;"></i></a>
-                        <a href="https://www.youtube.com/channel/UCY4PBN9HLG5oxjCtYXGvfeg" target="_blank"><i class="fab fa-youtube fa-2x mr-2 custom-hover2"style="color: #c4302b"></i></a>
-                        <a href="https://www.linkedin.com/company/14756318/" target="_blank"><i class="fab fa-linkedin fa-2x mr-2 custom-hover2"style="color:#0E76A8;"></i></a>
-                        <a href="#" target="_blank"><i class="fab fa-instagram fa-2x mr-2 custom-hover2"style="color:#C13584"></i></a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="col-md-12 border-top border-light mt-2 mb-2 text-center d-flex justify-content-center">
-                    <div class="col-md-8">
-                        <img class="img-fluid ssl-banner" src="/images/ssl.png">
-                    </div>
-                </div>
             </div>
         </div>
     </footer>
     <!-- End of Footer area -->
 
-    <section class="copyright" style="background: #f5b82f;">
-        <div class="container-fluid">
+    <section class="copyright">
+        <div class="container">
             <div class="row">
-                <div class="col-md-12 ">
-                    <div class="text-center text-white">
-                        &copy; Alokito Teachers {{ now()->year }}.
+                <div class="col-md-7">
+                    <div class="">
+                        Copyright &copy; Alokito Teachers {{ now()->year }}. All right reserved.
                     </div>
+                </div>
+                <div class="col-md-5">
+                    <ul>
+                        <li><a href="">Terms and Conditions</a></li>
+                        <li><a href="">Privacy Policy</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
